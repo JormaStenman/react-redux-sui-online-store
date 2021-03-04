dc7f7b8249b1b5814b0818b8f5610e8eb61daae8
Jorma Stenman <jorma.stenman@profitconsulting.fi>
2021-03-04 12:16:43 +0200

    use local reducer in ProductDetails
    Manage component state using a local reducer instead of multiple state variables.

--- src/features/products/ProductDetails.js
+++ src/features/products/ProductDetails.js
@@ -2,37 +2,59 @@ import {Card, Image, Message} from "semantic-ui-react";
 import {useRouteMatch} from "react-router-dom";
 import {productImageSrc} from "../../app/productUtils";
 import {currency} from "../../app/numberFormats";
-import {useEffect, useState} from "react";
+import {useEffect, useReducer} from "react";
 import Loading from "../loading/Loading";
 import client from "../../app/client";
+import {createAction, createReducer} from "@reduxjs/toolkit";
+
+const loadStart = createAction('productDetails/loadStart');
+const loadReady = createAction('productDetails/loadReady');
+const loadError = createAction('productDetails/loadError');
+
+const detailsReducer = createReducer({}, builder => {
+    builder
+        .addCase(loadStart, (state, action) => {
+            state.loadingStatus = 'loading';
+        })
+        .addCase(loadReady, (state, action) => {
+            state.loadingStatus = 'idle';
+            state.product = action.payload;
+        })
+        .addCase(loadError, (state, action) => {
+            state.loadingStatus = 'error';
+            state.error = action.payload;
+        });
+});
 
 export default function ProductDetails() {
     const match = useRouteMatch();
-    const [productId] = useState(parseInt(match.params.productId));
-    const [product, setProduct] = useState(null);
-    const [loadingStatus, setLoadingStatus] = useState('idle');
-    const [error, setError] = useState(null);
+    const [state, dispatch] = useReducer(detailsReducer, {
+        productId: match.params.productId,
+        product: null,
+        loadingStatus: 'idle',
+        error: null,
+    }, initialState => initialState);
 
     useEffect(() => {
         async function fetchData() {
-            return await client.getProductById(productId);
+            if (!state.product && state.loadingStatus === 'idle') {
+                dispatch(loadStart());
+                try {
+                    const result = await client.getProductById(parseInt(state.productId));
+                    dispatch(loadReady(result.product));
+                } catch (e) {
+                    dispatch(loadError(e.message || 'error loading product'))
+                }
+            }
         }
 
-        if (!product && loadingStatus === 'idle') {
-            setError(null);
-            setLoadingStatus('loading');
-            fetchData().then(result => {
-                setProduct(result.product);
-                setLoadingStatus('idle');
-                setError(null);
-            }).catch(e => {
-                setLoadingStatus('error');
-                setError(e.message || 'error loading product');
-            });
-        }
-    }, [product, loadingStatus, productId]);
+        // noinspection JSIgnoredPromiseFromCall
+        fetchData();
+        // eslint-disable-next-line
+    }, []);
 
     function productDetails() {
+        const product = state.product;
         if (product) {
             return (
                 <Card centered fluid>
@@ -55,13 +77,13 @@ export default function ProductDetails() {
         }
     }
 
-    switch (loadingStatus) {
+    switch (state.loadingStatus) {
         case 'idle':
             return productDetails();
         case 'loading':
-            return <Loading what={`product ${productId}`}/>
+            return <Loading what={`product ${state.productId}`}/>
         case 'error':
-            return <Message error content={error}/>
+            return <Message error content={state.error}/>
         default:
             return null;
     }
