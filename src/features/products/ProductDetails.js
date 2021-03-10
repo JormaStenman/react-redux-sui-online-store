3734d9e50fd96fb49c53bdee769976dc2075c84b
Jorma Stenman <jorma.stenman@profitconsulting.fi>
2021-03-10 11:53:45 +0200

    on-demand data loading in ProductDetails

--- src/features/products/ProductDetails.js
+++ src/features/products/ProductDetails.js
@@ -2,21 +2,58 @@ import {Button, Card, Container, Grid, Image, Message, Modal} from "semantic-ui-
 import {Link, useRouteMatch} from "react-router-dom";
 import {productImageSrc} from "../../app/productUtils";
 import {currency} from "../../app/numberFormats";
-import {useDispatch, useSelector} from "react-redux";
-import {productSelectors, selectProductsSlice} from "./productsSlice";
-import {useState} from "react";
+import {useDispatch} from "react-redux";
+import {fetchProductsByIds} from "./productsSlice";
+import {useEffect, useState} from "react";
 import {addToCart} from "../cart/cartSlice";
-import LoadingStatus from "../../app/LoadingStatus";
 import StoreModal from "../modal/StoreModal";
+import Loading from "../loading/Loading";
+import {unwrapResult} from "@reduxjs/toolkit";
 
 export default function ProductDetails() {
     const match = useRouteMatch();
-    // noinspection JSUnresolvedVariable
-    const productId = match.params.productId;
-    const product = useSelector(state => productSelectors.selectById(state, parseInt(productId)));
-    const loadingStatus = useSelector(state => selectProductsSlice(state).status);
+    const [product, setProduct] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [loadError, setLoadError] = useState('');
     const dispatch = useDispatch();
     const [addModalOpen, setAddModalOpen] = useState(false);
+
+    useEffect(() => {
+        let cancelled = false;
+
+        async function getProduct() {
+            setLoading(true);
+            setLoadError('');
+            try {
+                const response = unwrapResult(await dispatch(fetchProductsByIds([parseInt(match.params.productId)])));
+                if (!cancelled) {
+                    const products = response.products;
+                    if (products.length) {
+                        setProduct(products[0]);
+                    } else {
+                        setProduct(null);
+                        setLoadError('product not found');
+                    }
+                }
+            } catch (e) {
+                if (!cancelled) {
+                    setProduct(null);
+                    setLoadError(e);
+                }
+            } finally {
+                if (!cancelled) {
+                    setLoading(false);
+                }
+            }
+        }
+
+        // noinspection JSIgnoredPromiseFromCall
+        getProduct();
+
+        return () => {
+            cancelled = true;
+        };
+    }, [dispatch, match.params.productId]);
 
     function handleAddClick(productId) {
         dispatch(addToCart(productId));
@@ -60,10 +97,11 @@ export default function ProductDetails() {
                         <Grid.Row>
                             <Grid.Column>
                                 <Button.Group>
-                                    <Button icon='arrow alternate circle left outline' content='See more products' as={Link}
+                                    <Button icon='arrow alternate circle left outline' content='See more products'
+                                            as={Link}
                                             to='/products'/>
                                     <Button primary icon='add to cart' content='Add to cart'
-                                            onClick={() => handleAddClick(productId)}/>
+                                            onClick={() => handleAddClick(match.params.productId)}/>
                                 </Button.Group>
                             </Grid.Column>
                         </Grid.Row>
@@ -86,14 +124,15 @@ export default function ProductDetails() {
                 />
             </Container>
         );
-    } else {
-        if (loadingStatus === LoadingStatus.success) {
-            return (
-                <Message error>
-                    No product found with id {productId}.
-                </Message>
-            );
-        }
-        return null;
     }
+
+    if (loading) {
+        return <Loading what='product'/>;
+    }
+
+    if (loadError) {
+        return <Message error content={loadError}/>
+    }
+
+    return null;
 }
